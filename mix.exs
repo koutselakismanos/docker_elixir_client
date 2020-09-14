9446d06e0be52885b5e9747ceb67e9e809e17fb5
koutselakismanos <koutselakismanos@gmail.com>
2020-09-14 05:28:09 +0300

    add httpoison and poison

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule DockerElixirClient.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 1.6"}
+      {:httpoison, "~> 1.6"},
+      {:poison, "~> 3.1"}
     ]
   end
 end
