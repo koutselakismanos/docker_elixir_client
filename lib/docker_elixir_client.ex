3d1d8efa0f682f014a9cce843fbcfabc1ea44d83
koutselakismanos <koutselakismanos@gmail.com>
2020-09-14 05:29:20 +0300

    remove auto generated code

--- lib/docker_elixir_client.ex
+++ lib/docker_elixir_client.ex
@@ -1,18 +1,2 @@
 defmodule DockerElixirClient do
-  @moduledoc """
-  Documentation for `DockerElixirClient`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> DockerElixirClient.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
