6685e227eae42671634a8253c81760bcdc3944c2
koutselakismanos <koutselakismanos@gmail.com>
2020-09-14 05:28:50 +0300

    config docker_elixir_client_uri

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,13 @@
 use Mix.Config
 
+docker_engine_version = System.get_env("DOCKER_ENGINE_VERSION") || "1.40"
+
+docker_engine_uri =
+  System.get_env("DOCKER_ENGINE_URI") ||
+    "http+unix://%2fvar%2frun%2fdocker.sock/v#{docker_engine_version}"
+
+config :docker_elixir_client,
+       :uri,
+       docker_engine_uri
+
 import_config "#{Mix.env()}.exs"
