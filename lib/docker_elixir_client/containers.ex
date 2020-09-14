289dc1dc9ad819144f79939723ef17dfb954de0d
koutselakismanos <koutselakismanos@gmail.com>
2020-09-14 05:31:46 +0300

    add containers module

--- lib/docker_elixir_client/containers.ex
+++ lib/docker_elixir_client/containers.ex
@@ -1,1 +1,15 @@
+defmodule DockerElixirClient.Containers do
+  alias DockerElixirClient.HTTP
 
+  def all(host) when is_binary(host) do
+    HTTP.get(host <> "/containers/json", %{all: true})
+  end
+
+  def all(host, opts) when is_map(opts) do
+    HTTP.get(host <> "/containers/json", opts)
+  end
+
+  def create(host, opts) when is_map(opts) do
+    HTTP.post(host <> "/containers/create", opts)
+  end
+end
